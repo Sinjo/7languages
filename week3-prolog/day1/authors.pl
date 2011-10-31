99611f09b0c1046c95fb83b4cf5d5c9ed91d9c2b
Chris Sinjakli <chris@sinjakli.co.uk>
2011-10-31 23:40:15 +0000

    Day 1 of Prolog complete.

---
+++ week3-prolog/day1/authors.pl
@@ -0,0 +1,17 @@
+author('JK Rowling').
+author('Terry Pratchett').
+author('Bruce A. Tate').
+
+wrote('Harry Potter and the Chamber of Secrets', 'JK Rowling').
+wrote('Harry Potter and the Goblet of Fire', 'JK Rowling').
+wrote('Harry Potter and the Half Blood Prince', 'JK Rowling').
+wrote('Mort', 'Terry Pratchett').
+wrote('The Colour of Magic', 'Terry Pratchett').
+wrote('Seven Languages in Seven Weeks', 'Bruce A. Tate').
+
+/*
+ * The following will output all books written by 'JK Rowling'
+ *
+ * wrote(What, 'JK Rowling').
+ *
+ */
---
+++ week3-prolog/day1/musicians.pl
@@ -0,0 +1,19 @@
+plays('Dave Brubeck', 'piano').
+plays('John Coltrane', 'saxophone').
+plays('Matt Bellamy', 'guitar').
+plays('Matt Bellamy', 'piano').
+plays('Jimi Hendrix', 'guitar').
+plays('Tom Morello', 'guitar').
+
+style('Dave Brubeck', 'jazz').
+style('John Coltrane', 'jazz').
+style('Matt Bellamy', 'rock').
+style('Jimi Hendrix', 'rock').
+style('Tom Morello', 'rock').
+
+/*
+ * The following will output all musicians that play 'guitar'
+ *
+ * plays(Who, 'guitar').
+ *
+ */
