cb91d1662b829784f41ed5dad85e0a2a346e02bf
Chris Sinjakli <chris@sinjakli.co.uk>
2011-11-01 14:16:00 +0000

    Add used_in_genre rule, don't understand why it outputs duplicates.

--- week3-prolog/day1/musicians.pl
+++ week3-prolog/day1/musicians.pl
@@ -1,15 +1,17 @@
-plays('Dave Brubeck', 'piano').
-plays('John Coltrane', 'saxophone').
-plays('Matt Bellamy', 'guitar').
-plays('Matt Bellamy', 'piano').
-plays('Jimi Hendrix', 'guitar').
-plays('Tom Morello', 'guitar').
+plays('Dave Brubeck', piano).
+plays('John Coltrane', saxophone).
+plays('Matt Bellamy', guitar).
+plays('Matt Bellamy', piano).
+plays('Jimi Hendrix', guitar).
+plays('Tom Morello', guitar).
 
-style('Dave Brubeck', 'jazz').
-style('John Coltrane', 'jazz').
-style('Matt Bellamy', 'rock').
-style('Jimi Hendrix', 'rock').
-style('Tom Morello', 'rock').
+style('Dave Brubeck', jazz).
+style('John Coltrane', jazz).
+style('Matt Bellamy', rock).
+style('Jimi Hendrix', rock).
+style('Tom Morello', rock).
+
+used_in_genre(Instrument, Genre) :- plays(X, Instrument), style(X, Genre).
 
 /*
  * The following will output all musicians that play 'guitar'
